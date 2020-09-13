662562d5b73db887d459154c879130b89c298988
dreamer658 <calebyambu@yahoo.fr>
2020-09-13 02:20:48 +0200

    security details

--- lehana/lehana/settings.py
+++ lehana/lehana/settings.py
@@ -11,13 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 # Automatic reply email stuff
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'caldjango0@gmail.com'
-EMAIL_HOST_PASSWORD = 'Orangerouge10'
-
 
 
 import os
